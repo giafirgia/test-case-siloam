559df460421f36341dca3445fe34e48f8cf9b698
Grace Firgia <grace.firgia@bithealth.co.id>
2024-08-26 06:08:30 +0700

    Bypass URL

--- src/lib/url-config.ts
+++ src/lib/url-config.ts
@@ -1,3 +1,3 @@
 const apiSiloamHospitals = process.env.NEXT_PUBLIC_API_URL_SILOAM_HOSPITALS
 
-export const siloamHospitalsApi = `${apiSiloamHospitals}/api/v2`;
+export const siloamHospitalsApi = `https://uat-mysiloam-api.siloamhospitals.com/api/v2`;
