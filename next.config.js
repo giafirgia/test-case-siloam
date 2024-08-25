d6432bb03fae8dafa02a98cf54f562b0d0297f85
Grace Firgia <grace.firgia@bithealth.co.id>
2024-08-26 04:57:42 +0700

    update config

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
     images: {
       domains: ['api-mysiloam.siloamhospitals.com']
     },
+    output: 'export',
+    basePath: '/giafirgia.github.io',
+    assetPrefix: '/',
   }
